6a4899c7a5d10ca54b0fe513d4b6ed4372be8c75
Joao Nabais <nabaisaomar@gmail.com>
2020-09-15 17:07:15 +0200

    updated default template

--- karma.conf.js
+++ karma.conf.js
@@ -7,9 +7,20 @@ module.exports = function(config) {
         ],
         preprocessors: {
             'test/**/*.js': ['jshint', 'browserify'],
-            'src/**/*.js': ['jshint', 'browserify'] 
+            'src/**/*.js': ['jshint', 'browserify', 'coverage']
         },
         browsers: ['PhantomJS'],
+        reporters: ['coverage', 'coveralls'],
+          // optionally, configure the reporter
+          coverageReporter: {
+            dir : 'coverage/',
+            reporters: [
+                { type: 'text-summary' },
+                { type: 'json' },
+                { type: 'html' },
+                { type: 'lcov' }
+            ]
+          },
         browserify: {
         debug: true
         }
