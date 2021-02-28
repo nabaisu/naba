26bc68510a4d2df769dfe08a181218130f47a636
Joao Nabais <nabaisaomar@gmail.com>
2021-02-28 16:20:23 +0000

    finished dependency injection, going for providers now

--- __tests__/injector_spec.js
+++ __tests__/injector_spec.js
@@ -1,6 +1,7 @@
 import { createInjector } from "../src/injector";
 import { setupModuleLoader } from "../src/loader";
 import { APP_NAME, MODULES_NAME } from "../src/appdefaults";
+import { constant } from "lodash";
 
 describe('Injector', () => {
     beforeEach(() => {
@@ -142,8 +143,170 @@ describe('Injector', () => {
         expect(injector.invoke(fn, null, overrideObj)).toBe(42);
     })
 
+    describe('Annotate', () => {
+        it('returns the çinjector annotation of a function when it has one', () => {
+            var injector = createInjector([]);
+
+            var fn = function () { }
+            fn.çinject = ['a', 'b'];
+            expect(injector.annotate(fn)).toEqual(['a', 'b']);
+        })
+
+        it('returns the array-style annotations of a function', () => {
+            var injector = createInjector([]);
+
+            var fn = ['a', 'b', function () { }]
+            expect(injector.annotate(fn)).toEqual(['a', 'b']);
+        })
+
+        it('returns an empty array for a non-annotated 0-arg function', () => {
+            var injector = createInjector([]);
+
+            var fn = function () { }
+            expect(injector.annotate(fn)).toEqual([]);
+        })
+
+        it('returns annotations parsed from function args when not annotated', () => {
+            var injector = createInjector([]);
+
+            var fn = function (a, b) { }
+            expect(injector.annotate(fn)).toEqual(['a', 'b']);
+        })
+
+        it('strips commented arguments lists when parsing', () => {
+            var injector = createInjector([]);
+
+            var fn = function (a, /* b, */ c) { }
+            expect(injector.annotate(fn)).toEqual(['a', 'c']);
+        })
+        it('strips several commented arguments lists when parsing', () => {
+            var injector = createInjector([]);
+
+            var fn = function (a, /* b, */ c /*, d */) { }
+            expect(injector.annotate(fn)).toEqual(['a', 'c']);
+        })
+
+        it('strips // commented arguments lists when parsing', () => {
+            var injector = createInjector([]);
+
+            var fn = function (a, // b,
+                c
+            ) { }
+            expect(injector.annotate(fn)).toEqual(['a', 'c']);
+        })
+
+        it('strips surrounding underscores from argument names when parsing', () => {
+            var injector = createInjector([]);
+
+            var fn = function (a, _b_, _c, d_, an_argument) { }
+            expect(injector.annotate(fn)).toEqual(['a', 'b', '_c', 'd_', 'an_argument']);
+        })
+
+        it('throws when using a non-annotated fn in strict mode', () => {
+            var injector = createInjector([], true);
+
+            var fn = function (a, b, c) { }
+            expect(() => {
+                injector.annotate(fn)
+            }).toThrow();
+        })
+
+        it('invokes an array-annotated function with dependency injection', () => {
+            var module = window[APP_NAME][MODULES_NAME]('myModule', [])
+            module.constant('a', 20);
+            module.constant('b', 22);
+            var injector = createInjector(['myModule']);
+
+            var fn = ['a', 'b', function (aa, bb) { return aa + bb }]
+            expect(injector.invoke(fn)).toEqual(42);
+        })
+
+        it('invokes a non-annotated function with dependency injection', () => {
+            var module = window[APP_NAME][MODULES_NAME]('myModule', [])
+            module.constant('a', 20);
+            module.constant('b', 22);
+            var injector = createInjector(['myModule']);
+
+            var fn = function (a, b) { return a + b };
+            expect(injector.invoke(fn)).toEqual(42);
+        })
+
+        it('instantiates an annotated constructor function', () => {
+            var module = window[APP_NAME][MODULES_NAME]('myModule', [])
+            module.constant('a', 20);
+            module.constant('b', 22);
+            var injector = createInjector(['myModule']);
+
+            function Type(one, two) {
+                this.result = one + two;
+            }
+            Type.çinject = ['a', 'b'];
+
+            var instance = injector.instantiate(Type);
+            expect(instance.result).toBe(42);
+        })
+
+        it('instantiates an array annotation constructor function', () => {
+            var module = window[APP_NAME][MODULES_NAME]('myModule', [])
+            module.constant('a', 20);
+            module.constant('b', 22);
+            var injector = createInjector(['myModule']);
+
+            function Type(one, two) {
+                this.result = one + two;
+            }
+
+            var instance = injector.instantiate(['a', 'b', Type]);
+            expect(instance.result).toBe(42);
+        })
+
+        it('institiates a non-annotated constructor function', () => {
+            var module = window[APP_NAME][MODULES_NAME]('myModule', [])
+            module.constant('a', 20);
+            module.constant('b', 22);
+            var injector = createInjector(['myModule']);
+
+            function Type(a, b) {
+                this.result = a + b;
+            }
+
+            var instance = injector.instantiate(Type);
+            expect(instance.result).toBe(42);
+        })
+
+        it('uses the prototype of the constructor when instantiating', () => {
+            function BaseType() { }
+            BaseType.prototype.getValue = constant(42);
+
+            function Type(v) { this.v = this.getValue(); }
+            Type.prototype = BaseType.prototype;
+
+            window[APP_NAME][MODULES_NAME]('myModule', [])
+            var injector = createInjector(['myModule']);
+
+            var instance = injector.instantiate(Type);
+            expect(instance.v).toBe(42);
+        })
+
+        it('supports locals when instantiating', () => {
+            var module = window[APP_NAME][MODULES_NAME]('myModule', [])
+            module.constant('a', 20);
+            module.constant('b', 22);
+            var injector = createInjector(['myModule']);
+
+            var objToUse = { b: 5 }
+            function Type(a, b) {
+                this.result = a + b;
+            }
+
+            var instance = injector.instantiate(Type, objToUse);
+            expect(instance.result).toBe(25);
+        })
 
 
 
+
+
+    })
 
 })
--- src/injector.js
+++ src/injector.js
@@ -1,9 +1,14 @@
-import { forEach, isString, map } from "lodash";
+import { forEach, isArray, isFunction, isString, last, map } from "lodash";
 import { APP_NAME, MODULES_NAME } from "./appdefaults";
 
-function createInjector(modulesToLoad) {
+function createInjector(modulesToLoad, strictMode) {
+    var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
+    var FN_ARG = /^\s*(_?)(\S+?)\1\s*$/;
+    var STRIP_COMMENTS = /(\/\/.*$)|(\/\*.*?\*\/)/mg;
+
     var cache = {};
     var loadedModules = {};
+    var strictDI = (strictMode === true)
 
     var çprovide = {
         constant: function (key, value) {
@@ -26,7 +31,7 @@ function createInjector(modulesToLoad) {
     })
 
     function invoke(fn, self, locals) {
-        var args = map(fn.çinject, function (token) {
+        var args = map(annotate(fn), function (token) {
             if (isString(token)) {
                 return (locals && locals.hasOwnProperty(token)) ?
                     locals[token] :
@@ -35,7 +40,36 @@ function createInjector(modulesToLoad) {
                 throw `Incorrect injection token! Expected a string, got: ${token}`
             }
         });
+
+        if (isArray(fn)) {
+            fn = last(fn);
+        }
         return fn.apply(self, args);
+    }
+
+    function annotate(fn) {
+        if (isArray(fn)) {
+            return fn.slice(0, fn.length - 1);
+        } else if (fn.çinject) {
+            return fn.çinject
+        } else if (!fn.length) {
+            return [];
+        } else {
+            if (strictDI) throw 'fn is not using explicit annotation and cannot be invoked in strict mode'
+            var source = fn.toString().replace(STRIP_COMMENTS, '');
+            var argDeclaration = source.match(FN_ARGS); //?
+            return map(argDeclaration[1].split(','), arg => {
+                return arg.match(FN_ARG)[2]; //?
+            })
+        }
+    }
+
+    function instantiate(Type, locals) {
+        var unwrappedType = isArray(Type) ? last(Type) : Type;
+        var instance = Object.create(unwrappedType.prototype);
+        invoke(Type, instance, locals);
+        return instance;
+        
     }
 
     return {
@@ -45,7 +79,9 @@ function createInjector(modulesToLoad) {
         get(value) {
             return cache[value];
         },
-        invoke: invoke
+        invoke: invoke,
+        annotate: annotate,
+        instantiate: instantiate
     }
 }
 
