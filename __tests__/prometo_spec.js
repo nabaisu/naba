2836e78c5dc494176bfff2c1b1a8a765109cc30a
Joao Nabais <nabaisaomar@gmail.com>
2021-03-25 12:08:03 +0000

    finished handling promise exceptions

--- __tests__/prometo_spec.js
+++ __tests__/prometo_spec.js
@@ -1,6 +1,7 @@
 import { APP_NAME, MODULES_NAME, ÇPÃ_NAME } from "../src/appdefaults";
 import { publishExternalAPI } from "../src/naba_public";
 import { createInjector } from "../src/injector";
+import { noop } from "lodash";
 
 
 describe('naba Public', () => {
@@ -69,9 +70,298 @@ describe('naba Public', () => {
 
         d.resolve(42);
         çrootScope.çapply();
-        
+
         expect(promiseSpy).toHaveBeenCalledWith(42);
     });
+    it(`may only be resolved once`, () => {
+        var d = çprometo.defer();
+
+        var promiseSpy = jest.fn();
+        d.promise.then(promiseSpy);
+
+        d.resolve(42);
+        d.resolve(43);
+
+        çrootScope.çapply();
+
+        expect(promiseSpy).toHaveBeenCalledWith(42);
+        expect(promiseSpy).toHaveBeenCalledTimes(1);
+
+    });
+
+    it(`may only ever be resolved once`, () => {
+        var d = çprometo.defer();
+
+        var promiseSpy = jest.fn();
+        d.promise.then(promiseSpy);
+
+        d.resolve(42);
+        çrootScope.çapply();
+        expect(promiseSpy).toHaveBeenCalledWith(42);
+
+        d.resolve(43);
+        çrootScope.çapply();
+
+        expect(promiseSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it(`resolves a listener added after resolution`, () => {
+        var d = çprometo.defer();
+        d.resolve(42);
+        çrootScope.çapply();
+
+        var promiseSpy = jest.fn();
+        d.promise.then(promiseSpy);
+        çrootScope.çapply();
+
+        expect(promiseSpy).toHaveBeenCalledWith(42);
+    });
+    it(`may have multiple callbacks`, () => {
+        var d = çprometo.defer();
+        var promiseSpy1 = jest.fn();
+        var promiseSpy2 = jest.fn();
+
+        d.promise.then(promiseSpy1);
+        d.promise.then(promiseSpy2);
+
+        d.resolve(42);
+        çrootScope.çapply();
+
+        expect(promiseSpy1).toHaveBeenCalledWith(42);
+        expect(promiseSpy2).toHaveBeenCalledWith(42);
+    });
+
+    it(`invokes each callback once`, () => {
+        var d = çprometo.defer();
+        var promiseSpy1 = jest.fn();
+        var promiseSpy2 = jest.fn();
+
+        d.promise.then(promiseSpy1);
+        d.resolve(42);
+        çrootScope.çapply();
+        expect(promiseSpy1).toHaveBeenCalledTimes(1);
+        expect(promiseSpy2).toHaveBeenCalledTimes(0);
+
+        d.promise.then(promiseSpy2);
+        expect(promiseSpy1).toHaveBeenCalledTimes(1);
+        expect(promiseSpy2).toHaveBeenCalledTimes(0);
+
+        çrootScope.çapply();
+        expect(promiseSpy1).toHaveBeenCalledTimes(1);
+        expect(promiseSpy2).toHaveBeenCalledTimes(1);
+    });
+
+    it(`can reject a deferred`, () => {
+        var d = çprometo.defer();
+        var successFn = jest.fn();
+        var errorFn = jest.fn();
+
+        d.promise.then(successFn, errorFn);
+        d.reject('fail');
+        çrootScope.çapply();
+
+        expect(successFn).not.toHaveBeenCalled();
+        expect(errorFn).toHaveBeenCalledWith('fail');
+    });
+
+    it(`can reject just once`, () => {
+        var d = çprometo.defer();
+        var successFn = jest.fn();
+        var errorFn = jest.fn();
+
+        d.promise.then(successFn, errorFn);
+        d.reject('fail');
+        çrootScope.çapply();
+        expect(errorFn).toHaveBeenCalledTimes(1);
+
+        d.reject('fail again');
+        çrootScope.çapply();
+        expect(errorFn).toHaveBeenCalledTimes(1);
+    });
+    it(`cannot fulfill a promise once rejected`, () => {
+        var d = çprometo.defer();
+
+        var successFn = jest.fn();
+        var errorFn = jest.fn();
+        d.promise.then(successFn, errorFn);
+
+        d.reject('fail');
+        çrootScope.çapply();
+
+        d.resolve('yes');
+        çrootScope.çapply();
+
+        expect(errorFn).toHaveBeenCalledTimes(1);
+        expect(successFn).not.toHaveBeenCalled();
+    });
+
+    it(`does not require a failure handler each time`, () => {
+        var d = çprometo.defer();
+
+        var successFn = jest.fn();
+        var errorFn = jest.fn();
+        d.promise.then(successFn);
+        d.promise.then(null, errorFn);
+
+        d.reject('fail');
+        çrootScope.çapply();
+
+        expect(errorFn).toHaveBeenCalledWith('fail');
+    });
+    it(`does not require a success handler each time`, () => {
+        var d = çprometo.defer();
+
+        var successFn = jest.fn();
+        var errorFn = jest.fn();
+        d.promise.then(successFn);
+        d.promise.then(null, errorFn);
+
+        d.resolve('ok');
+        çrootScope.çapply();
+
+        expect(successFn).toHaveBeenCalledWith('ok');
+    });
+
+    it(`can register rejection handler with catch`, () => {
+        var d = çprometo.defer();
+
+        var errorFn = jest.fn();
+        d.promise.catch(errorFn);
+        d.reject('fail');
+        çrootScope.çapply();
+
+        expect(errorFn).toHaveBeenCalled();
+    });
+
+    it(`invokes the finally handler when fulfilled`, () => {
+        var d = çprometo.defer();
+
+        var finallyFn = jest.fn();
+        d.promise.finally(finallyFn);
+        d.resolve(42);
+        çrootScope.çapply();
+
+        expect(finallyFn).toHaveBeenCalled();
+    });
+    it(`invokes the finally handler when rejected`, () => {
+        var d = çprometo.defer();
+
+        var finallyFn = jest.fn();
+        d.promise.finally(finallyFn);
+        d.reject('fail');
+        çrootScope.çapply();
+
+        expect(finallyFn).toHaveBeenCalled();
+    });
+
+    it(`allows chaining handlers`, () => {
+        var d = çprometo.defer();
+
+        var firstFn = function (value) { return value + 1 };
+        var successFn = jest.fn();
+        d.promise
+            .then(firstFn)
+            .then(firstFn)
+            .then(successFn);
+
+        d.resolve(40);
+        çrootScope.çapply();
+
+        expect(successFn).toHaveBeenCalledWith(42);
+    });
+
+    it(`does not modify original resolution in chains`, () => {
+        var d = çprometo.defer();
+
+        var firstFn = function (value) { return value + 1 };
+        var successFn = jest.fn();
+        d.promise
+            .then(firstFn)
+            .then(firstFn);
+        d.promise.then(successFn);
+
+        d.resolve(42);
+        çrootScope.çapply();
+
+        expect(successFn).toHaveBeenCalledWith(42);
+    });
+
+    it(`catches rejection on chain handler`, () => {
+        var d = çprometo.defer();
+
+        var rejectedFn = jest.fn();
+        d.promise
+            .then(noop)
+            .catch(rejectedFn);
+
+        d.reject('fail');
+        çrootScope.çapply();
+
+        expect(rejectedFn).toHaveBeenCalledWith('fail');
+    });
+
+    it(`fulfills on chain handler`, () => {
+        var d = çprometo.defer();
+
+        var fulfilledFn = jest.fn();
+        d.promise
+            .catch(noop)
+            .then(fulfilledFn);
+
+        d.resolve(42);
+        çrootScope.çapply();
+
+        expect(fulfilledFn).toHaveBeenCalledWith(42);
+    });
+
+    it(`treats catch return value as resolution`, () => {
+        var d = çprometo.defer();
+
+        var catchFn = function () { return 42 };
+        var successFn = jest.fn();
+        d.promise
+            .catch(catchFn)
+            .then(successFn);
+
+        d.reject('fail');
+        çrootScope.çapply();
+
+        expect(successFn).toHaveBeenCalledWith(42);
+    });
+
+    it(`it rejects chained promise when handler throws`, () => {
+        var d = çprometo.defer();
+
+        var errorFn = function () { throw 'errorr' };
+        var rejectedFn = jest.fn();
+        d.promise
+            .then(errorFn)
+            .catch(rejectedFn);
+
+        d.resolve(42);
+        çrootScope.çapply();
+
+        expect(rejectedFn).toHaveBeenCalledWith('errorr');
+    });
+
+    it(`it does not reject current promise when handler throws`, () => {
+        var d = çprometo.defer();
+
+        var errorFn = function () { throw 'fail' };
+        var rejectedFn = jest.fn();
+
+        d.promise.then(errorFn);
+        d.promise.catch(rejectedFn);
+
+        d.resolve(42);
+        çrootScope.çapply();
+
+        expect(rejectedFn).not.toHaveBeenCalled();
+    });
+
+
+
+
 
 
 })
--- src/prometo.js
+++ src/prometo.js
@@ -1,35 +1,85 @@
-function çPrometoProvider(){
-    this.çget = ['çrootScope',function(çrootScope){
+import { forEach, isFunction } from "lodash";
 
-        function Deferred(){
+function çPrometoProvider() {
+    this.çget = ['çrootScope', function (çrootScope) {
+
+        function Deferred() {
             this.promise = new Promise();
         }
-        Deferred.prototype.resolve = function(value){
+        Deferred.prototype.resolve = function (value) {
             // to make some tests work, instead of the eval async, to run something in the future, one can call setTimeout with 0
+            if (this.promise.ççstate.status) {
+                return
+            }
             this.promise.ççstate.value = value;
+            this.promise.ççstate.status = 1; // means resolved
             scheduleProcessQueue(this.promise.ççstate);
         }
 
-        function Promise(){
+        Deferred.prototype.reject = function (reason) {
+            if (this.promise.ççstate.status) {
+                return
+            }
+            this.promise.ççstate.value = reason; // means rejected
+            this.promise.ççstate.status = 2; // means rejected
+            scheduleProcessQueue(this.promise.ççstate);
+        }
+
+        function Promise() {
             this.ççstate = {}
         }
 
-        Promise.prototype.then = function(onFulfilled){
-            this.ççstate.pending = onFulfilled;
+        Promise.prototype.then = function (onFulfilled, onRejected) {
+            var result = new Deferred();
+            this.ççstate.pending = this.ççstate.pending || [];
+            this.ççstate.pending.push([result, onFulfilled, onRejected]); // isto é com base no status index
+            if (this.ççstate.status > 0) {
+                scheduleProcessQueue(this.ççstate);
+            }
+            return result.promise;
         }
 
-        function defer(){
+        Promise.prototype.catch = function (onRejected) {
+            return this.then(null, onRejected);
+        }
+
+        Promise.prototype.finally = function (callback) {
+            return this.then(function(){
+                callback();
+            }, function () {
+                callback();
+            })
+            // basicamente assina-se ao onFullfilled e ao onRejected o chamamento da callback
+        }
+
+        function defer() {
             return new Deferred();
         }
 
         function scheduleProcessQueue(state) {
-            çrootScope.çevalAsync(function(){
+            çrootScope.çevalAsync(function () {
                 processQueue(state);
             });
         }
 
         function processQueue(state) {
-            state.pending(state.value);
+            var pending = state.pending;
+            state.pending = undefined;
+            forEach(pending, function(handlers){
+                var deferred = handlers[0]; 
+                var fn = handlers[state.status]; // isto é interessante porque ele vai passando o state entre eles
+                try {
+                    if (isFunction(fn)) {
+                        deferred.resolve(fn(state.value)); // aqui já meteu recursão para xuxu
+                    } else if ( state.status === 1){
+                        deferred.resolve(state.value); // aqui já meteu recursão para xuxu
+                    } else {
+                        deferred.reject(state.value);
+                    }                    
+                } catch (error) {
+                    deferred.reject(error);
+                }
+            })
         }
 
         return {
